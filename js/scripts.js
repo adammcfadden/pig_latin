90a39e4b9ccb5ecc8a07e02f7440685f8e2d9397
Adam McFadden & Ian MacDonald <student@epicodus.com>
2015-05-27 11:42:03 -0700

    refactor code, all specs except y are still passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,5 @@
 var translate = function(word) {
-  var translatedWord = word
-  if (!(word.charAt(0).match("q"))) {
-    translatedWord = moveConsonants(word);
-  } else {
-    translatedWord = moveQU(translatedWord);
-    translatedWord = moveConsonants(translatedWord);
-  }
+  var translatedWord = moveConsonants(word);
   return addAy(translatedWord);
 };
 
@@ -16,22 +10,30 @@ var addAy = function(word) {
 var moveConsonants = function(word) {
   if (!(word.charAt(0).match(/[aeiou]/))) {
     if ((word.match(/\b[^aeio]*/)).join().includes("qu")) {
-      word = (word.split(/\b[^q]*/)[1] + word.match(/\b[^q]*/));
-      word = moveQU(word);
+      if (word.charAt(0).match("q")) {
+        word = moveQU(word);
+        word = moveConsonants(word);
+      } else {
+        word = (word.split(/\b[^q]*/)[1] + word.match(/\b[^q]*/));
+        word = moveQU(word);
+      }
     } else {
+      word = moveQU(word);
       word = (word.split(/\b[^aeiou]*/)[1] + word.match(/\b[^aeiou]*/));
     }
   }
-  return word
+  return word;
 };
 
+
 var moveQU = function(word) {
-  if (word.charAt(1).match("u")) {
-    var slicedWord = word.slice(2)
-    return slicedWord = slicedWord + "qu"
-  }
-  else {
-    var slicedWord = word.slice(1)
-    return slicedWord = slicedWord + "q"
+  if (word.replace(word.slice(2), "").includes("qu")) {
+    var slicedWord = word.slice(2);
+    return slicedWord = slicedWord + "qu";
+  } else if (word.charAt(0).match("q")) {
+    var slicedWord = word.slice(1);
+    return slicedWord = slicedWord + "q";
+  } else {
+    return word;
   }
 };
